c5cfbe5a271ecb55d31ccecad51ab80cfa7ef540
Oshiole Calistus Inaboya <calistus@userank.com>
2026-01-06 11:27:35 +0100

    d

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,8 @@ dotenv.config({ path: "retest.env" });
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.ts',
+    supportFile: 'cypress/support/e2e.ts',
     supportFile: false,
     setupNodeEvents(on, config) {
       allureWriter(on, config);
