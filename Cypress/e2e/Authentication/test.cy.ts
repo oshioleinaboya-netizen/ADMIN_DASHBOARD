47025eb657c5b3d84596237452f17c3ce742a713
Oshiole Calistus Inaboya <calistus@userank.com>
2026-01-06 15:26:45 +0100

    jk

--- Cypress/e2e/Authentication/test.cy.ts
+++ Cypress/e2e/Authentication/test.cy.ts
@@ -1,4 +1,5 @@
 it("can reach homepage", () => {
-  cy.visit("/");
-  cy.contains("Login", { timeout: 20000 }).should("be.visible");
+    cy.visit("/");
+    cy.wait(5000); // wait 5 seconds
+    cy.screenshot("homepage"); // save a screenshot
 });
