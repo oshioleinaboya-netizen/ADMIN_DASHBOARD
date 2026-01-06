c1b4b035cf64c2aa0ad27bb2087fa565d6bb1cbe
Oshiole Calistus Inaboya <calistus@userank.com>
2026-01-06 10:49:04 +0100

    Switch Cypress config to JS to fix tsx loader issue

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+
+dotenv.config({ path: "retest.env" });
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+
+      config.env = {
+        ...config.env,
+        ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+        RANK_LINK: process.env.RANK_LINK,
+      };
+
+      return config;
+    },
+    baseUrl: process.env.RANK_LINK,
+  },
+});
