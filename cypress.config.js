5a860621d01a3cf612be0ac6ae5bb80bb5b6e909
Gboluwa Adeyemi <gboluwa@heymoni.com>
2026-01-08 13:12:19 +0100

    fix: cypress config change to use lowercase cypress folder

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
-const dotenv = require("dotenv");
-const { defineConfig } = require("cypress");
-const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+const dotenv = require('dotenv');
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 if (!process.env.CI) {
-  dotenv.config({ path: "retest.env" });
+  dotenv.config({ path: 'retest.env' });
 }
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'Cypress/e2e/**/*.cy.{ts,js}',
-    supportFile: "Cypress/support/e2e.ts",
+    specPattern: 'cypress/e2e/**/*.cy.{ts,js}',
+    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       allureWriter(on, config);
 
