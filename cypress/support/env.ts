a0bc9809c146c661b1417c8c09160ccfd0b22ee3
Oshiole Calistus Inaboya <calistus@userank.com>
2026-03-31 14:22:49 +0100

    x

--- cypress/support/env.ts
+++
@@ -1,4 +0,0 @@
-export const adminEmail = Cypress.env("ADMIN_EMAIL");
-export const adminPassword = Cypress.env("ADMIN_PASSWORD");
-export const roscaUserPhone = Cypress.env('ROSCA_USER_PHONE');
-// export const rankLink = Cypress.env("RANK_LINK");
